56064dfb011348f6ea98e071b1085120898bfa2a
bram-n <bnutt@macalester.edu>
2023-07-11 22:31:40 -0500

    save

---
+++ Components/Home.js
@@ -0,0 +1,21 @@
+import React from React
+import Navbar from "./Navbar";
+const Home = () => {
+    return (
+    <div class= 'home-container'> 
+    <Navbar/>
+     <div className='home-banner-container'>
+        <div className = 'home-text-section'>
+            <h1 className = "primary-heading">
+                Search for the Right Internship for You!
+            </h1>
+            <p className='primary-text'>
+                To be filled later
+            </p>
+
+        </div>
+     </div>
+    </div>
+
+    );
+}
