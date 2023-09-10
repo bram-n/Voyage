fc7612d6945faafb54971d4745504ef0a5ad744c
bram-n <bnutt@macalester.edu>
2023-09-10 11:08:28 -0500

    Updated code

--- StudentBaseInternship/src/Pages/Internship.tsx
+++ StudentBaseInternship/src/Pages/Internship.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Scroll from '../Components/Scroll'
+import NavBar from '../Components/NavBar'
 
 export default function Internship() {
   return (
+    <>
+    <NavBar/>
     <Scroll/>
+    </>
   )
 }
