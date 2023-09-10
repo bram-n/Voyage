a996c630b7bf014efda4f9552a1e83d02d2aef6f
bram-n <bnutt@macalester.edu>
2023-09-10 12:24:44 -0500

    New code with infinite scroll table

--- StudentBaseInternship/src/Components/Scroll.tsx
+++ StudentBaseInternship/src/Components/Scroll.tsx
@@ -1,77 +1,75 @@
+// ScrollTable.js
+
 import React, { useEffect, useState } from 'react';
-
-const tableStyle = {
-    border: "1px solid green",
-    margin: 12,
-    padding: 8,
-};
-
+import '../Aesthetics/ScrollTable.css'; // Import the CSS file
 const itemsPerPage = 20;
 
 const ScrollTable = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [dataSource, setDataSource] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dataSource, setDataSource] = useState([]);
 
-    useEffect(() => {
-        // Generate data for the current page
-        const startIndex = (currentPage - 1) * itemsPerPage;
-        const endIndex = startIndex + itemsPerPage;
-        const newData = Array.from({ length: itemsPerPage }).map((_, index) => ({
-            company: startIndex + index + 1,
-            title: `Data ${startIndex + index + 1}`,
-            date:  startIndex + index + 1,
-            wage:  startIndex + index + 1,
-        }));
-        setDataSource(newData);
-    }, [currentPage]);
+  useEffect(() => {
+    // Generate data for the current page
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const newData = Array.from({ length: itemsPerPage }).map((_, index) => ({
+      company: startIndex + index + 1,
+      title: `Data ${startIndex + index + 1}`,
+      date: startIndex + index + 1,
+      wage: startIndex + index + 1,
+    }));
+    setDataSource(newData);
+  }, [currentPage]);
 
-    const totalPages = Math.ceil(200 / itemsPerPage);
+  const totalPages = Math.ceil(200 / itemsPerPage);
 
-    const handleNextPage = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-    const handlePrevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
-    return (
-        <div className="ScrollTable">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Company</th>
-                        <th>Job Title</th>
-                        <th>Estimated Hourly Wage</th>
-                        <th>Date Posted</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {dataSource.map((item) => (
-                        <tr key={item.company} style={tableStyle}>
-                            <td>{item.company}</td>
-                            <td>{item.title}</td>
-                            <td>{item.wage}</td>
-                            <td>{item.date}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div className="pagination">
-                <button onClick={handlePrevPage} disabled={currentPage === 1}>
-                    Previous
-                </button>
-                <span>Page {currentPage} of {totalPages}</span>
-                <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-                    Next
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="ScrollTable">
+      <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Job Title</th>
+            <th>Estimated Hourly Wage</th>
+            <th>Date Posted</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataSource.map((item) => (
+            <tr key={item.company}>
+              <td>{item.company}</td>
+              <td>{item.title}</td>
+              <td>{item.wage}</td>
+              <td>{item.date}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ScrollTable;
