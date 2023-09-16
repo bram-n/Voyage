a15769c11a3ee548d0edded92551086ceede377e
LongAtGitHub <ltruong@macalester.edu>
2023-09-15 23:41:47 -0500

    Style for navbar

--- StudentBaseInternship/src/Components/NavBar.tsx
+++ StudentBaseInternship/src/Components/NavBar.tsx
@@ -3,6 +3,7 @@ import {Navbar, Nav, Container, Button}  from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import {useAuth} from "../Contexts/AuthContext"
 import './NavBarStyle.css'
+import imageLogo from '../assets/Sb-2.png'
 
 // NavBar with routing applied
 
@@ -11,9 +12,16 @@ export default function NavBar() {
   return (
     <Navbar bg="light" data-bs-theme="light" className='yellow-background'>
         <Container>
-          <NavLink to="/" className="nav-link" >Student Internship</NavLink>
-          <Nav className="">
-            <NavLink to="/internship" className="nav-link text-style">Internship</NavLink>
+          <NavLink to="/" className="logo-div" >
+            <img 
+              src={imageLogo} 
+              alt="Internship Logo" 
+              className="image-style"
+              style={{ maxWidth: '100px', maxHeight: '100px' }} 
+            />
+          </NavLink>
+          <Nav className='nav-right-side-component'>
+            <NavLink to="/internship" className="nav-link text-style ">Internship</NavLink>
 
             { isAuthenticated ? 
               // if user logs in
@@ -23,9 +31,8 @@ export default function NavBar() {
               </>
               :
               // if user has not logged in
-              <NavLink to="/signup" className="nav-link signup-box-style">Sign Up</NavLink>
+              <NavLink to="/signup" className="signup-box-style">Sign Up</NavLink>
             }
-            
           </Nav>
         </Container>
       </Navbar>
