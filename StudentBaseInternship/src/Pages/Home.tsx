1e92f6f218a4bd61d5ccf70ced8d0b657ef4ee03
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2024-02-06 23:00:51 -0600

    Fix Home CSS

--- StudentBaseInternship/src/Pages/Home.tsx
+++ StudentBaseInternship/src/Pages/Home.tsx
@@ -11,29 +11,32 @@ export default function Home() {
   console.log(currentUser?.toJSON);
   return (
     <>
-      <NavBar/>
-      <Container>
-      <Row>
-      <Col xs={12} md={6} className = "column">
-        <br/>
-        <h1 className="text-center">CONTINUE YOUR VOYAGE</h1>
-        <h2 className = "text-paragraph">Embark on a journey towards your dream career with Voyage, exploring endless opportunities and navigating the dynamic world of work with confidence.</h2>
-        <br/>
-        <NavLink to="/job" style={{ textDecoration: 'none' }}>
-          <div className="d-grid gap-2">
-            <Button variant="primary" size="lg" className = "explore-jobs">
-              Explore Jobs
-            </Button>
-          </div>
-        </NavLink>
-      </Col>
-      <Col xs={12} md={6}>
-        <img src={WebImage} alt="Placeholder Image" className="img-work1" />
-      </Col>
-    </Row>
-    <br></br>
-    </Container>
+      <NavBar />
+      <Row style ={{width : '100%', height: '40%'}}>
+         <Col xs={12} md={6} className = "column">
+          <h1 className="text-center">CONTINUE YOUR VOYAGE</h1>
+          <h2 className = "text-paragraph">Embark on a journey towards your dream career with Voyage, exploring endless opportunities and navigating the dynamic world of work with confidence.</h2>
+          <br/>
+          <NavLink to="/job" style={{ textDecoration: 'none' }}>
+            <div className="d-grid gap-2">
+              <Button variant="primary" size="lg" className = "explore-jobs">
+                Explore Jobs
+              </Button>
+            </div>
+          </NavLink>
+        </Col> 
 
+        <Col xs={12} md={6}>
+         <img src={WebImage} alt="Placeholder Image" className="img-work1" /> 
+        
+        </Col>
+        <div style={{backgroundColor: "#eccfaf"}}>{'\u00A0'}</div> 
+        {/* trick to hide white line */}
+      
+    </Row>
+
+    
+      
     </>
   );
 }
